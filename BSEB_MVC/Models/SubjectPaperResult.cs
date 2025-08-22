d16ad8e28669a7956f5d293ebaa53c7b2fd6986f
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-08-22 16:54:39 +0530

    add new class for subject paper

---
+++ BSEB_MVC/Models/SubjectPaperResult.cs
@@ -0,0 +1,21 @@
+﻿namespace BSEB_MVC.Models
+{
+    public class SubjectPaperResult
+    {
+        public int? SubjectCode { get; set; }
+        public string? SubjectName { get; set; }
+
+        // New properties to align with the error message
+        public string? GroupName { get; set; }
+        public string? SubjectPaperName { get; set; }
+        public int? SubjectPaperCode { get; set; }
+
+        // Extra properties, as seen in your input (if you want to use these as well)
+        public string? Name1 { get; set; }
+        public int? Code1 { get; set; }
+        public string? Name2 { get; set; }
+        public int? Code2 { get; set; }
+        public string? Name3 { get; set; }
+        public int? Code3 { get; set; }
+    }
+}

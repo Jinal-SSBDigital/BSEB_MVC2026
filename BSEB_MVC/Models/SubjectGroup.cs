78a030fecaa66c410d230d3e266a54d28afd2e8a
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-08-22 16:54:21 +0530

    add new class for subject grp

---
+++ BSEB_MVC/Models/SubjectGroup.cs
@@ -0,0 +1,8 @@
+﻿namespace BSEB_MVC.Models
+{
+    public class SubjectGroup
+    {
+        public SubjectPaperResult Group1Subject { get; set; } = new SubjectPaperResult();
+        public SubjectPaperResult Group2Subject { get; set; } = new SubjectPaperResult();
+    }
+}

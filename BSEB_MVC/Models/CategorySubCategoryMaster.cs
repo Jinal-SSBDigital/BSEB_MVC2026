3ffaf1ef2a7ff22707204e26fd481561fb0b8d1b
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-10-03 17:44:19 +0530

    Add subcategory

---
+++ BSEB_MVC/Models/CategorySubCategoryMaster.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.DTOs
+{
+    public class CategorySubCategoryMaster
+    {
+        public int CatId { get; set; }
+        public string CategoryName { get; set; }
+        public string SubCategoryName { get; set; }
+        public int SubCatId { get; set; }
+
+    }
+}

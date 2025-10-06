ef5da7f3bfb5d7a5b84e32322de657c557e5a351
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-10-06 17:21:31 +0530

    changes

---
+++ BSEB_MVC/Models/ProductDetailsFileMaster.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.DTOs
+{
+    public class ProductDetailsFileMaster
+    {
+        public string ProductName { get; set; }
+
+        public string Price { get; set; }
+
+        public string StockQuantity { get; set; }
+
+       
+    }
+    public class ProductFileMaster
+    {
+        public IFormFile Filename { get; set; }
+    }
+}

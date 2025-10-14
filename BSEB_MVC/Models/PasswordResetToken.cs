14175f6eae8f9211e8e03e23af3ba3ff782df5e2
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-10-14 16:40:08 +0530

    Reset Password changes

---
+++ BSEB_MVC/Models/PasswordResetToken.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTOs
+{
+    public class PasswordResetToken
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        //public DateTime Expiration { get; set; }
+        //public bool IsUsed { get; set; } = false;
+    }
+}

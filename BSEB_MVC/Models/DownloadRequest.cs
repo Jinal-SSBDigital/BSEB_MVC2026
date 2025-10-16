33354798f5ee1fdda211ab5b0bb18bd91e7c1517
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-10-16 17:27:29 +0530

    changes

--- BSEB_MVC/Models/DownloadRequest.cs
+++ BSEB_MVC/Models/DownloadRequest.cs
@@ -4,6 +4,8 @@
     {
         // Changed to List<int> to match JavaScript's parseInt
         public List<int> StudentIds { get; set; }
+        public List<int> FacultyIds { get; set; }
+        public List<int> ExamIds { get; set; }
         public string collegeId { get; set; }
         // Changed to int to match the data-faculty attribute and form input
         public int Faculty { get; set; }

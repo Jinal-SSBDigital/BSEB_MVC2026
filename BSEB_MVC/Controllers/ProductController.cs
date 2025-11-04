a6abff7deb8654112c21c9137d1a6995e59227b7
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-11-04 17:28:27 +0530

    changes

--- BSEB_MVC/Controllers/ProductController.cs
+++ BSEB_MVC/Controllers/ProductController.cs
@@ -1,41 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using WebApplication1.Services;
+//using WebApplication1.Services;
 
 namespace WebApplication1.Controllers
 {
     public class ProductController : Controller
     {
-        private readonly IActiveLogService _activeLogService;
-        private readonly ILogger<ActiveLogController> _logger;
+        //private readonly IActiveLogService _activeLogService;
+        //private readonly ILogger<ActiveLogController> _logger;
 
-        public ProductController(IActiveLogService activeLogService, ILogger<ActiveLogController> logger)
-        {
-            _activeLogService = activeLogService;
-            _logger = logger;
-        }
-        [HttpPost("activelog")]
-        public async Task<IActionResult> Activelog([FromForm] DTOs.ActiveLogDTO activeLog)
-        {
-            try
-            {
-                var result = await _activeLogService.UserActiveLogAsync(activeLog, HttpContext);
-                if (result)
-                {
-                    _logger.LogInformation("User activelog: {type}", activeLog.activitytype);
-                    return Ok(new { success = true, message = "User Active LOg successfully" });
-                }
-
-
+        //public ProductController(IActiveLogService activeLogService, ILogger<ActiveLogController> logger)
+        //{
+        //    _activeLogService = activeLogService;
+        //    _logger = logger;
+        //}
+        //[HttpPost("activelog")]
+        //public async Task<IActionResult> Activelog([FromForm] DTOs.ActiveLogDTO activeLog)
+        //{
+        //    try
+        //    {
+        //        var result = await _activeLogService.UserActiveLogAsync(activeLog, HttpContext);
+        //        if (result)
+        //        {
+        //            _logger.LogInformation("User activelog: {type}", activeLog.activitytype);
+        //            return Ok(new { success = true, message = "User Active LOg successfully" });
+        //        }
 
 
-                _logger.LogWarning("Registration failed for user activelog: {type}", activeLog.activitytype);
-                return BadRequest(new { success = false, message = "Registration failed" });
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Exception during registration for user activelog: {type}", activeLog.activitytype);
-                return StatusCode(500, new { success = false, message = "Internal server error" });
-            }
-        }
+
+
+        //        _logger.LogWarning("Registration failed for user activelog: {type}", activeLog.activitytype);
+        //        return BadRequest(new { success = false, message = "Registration failed" });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(ex, "Exception during registration for user activelog: {type}", activeLog.activitytype);
+        //        return StatusCode(500, new { success = false, message = "Internal server error" });
+        //    }
+        //}
     }
 }

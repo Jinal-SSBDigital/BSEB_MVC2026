aa12e6e6398fa421ef2d05a375037b172ae2b8aa
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-11-05 17:41:37 +0530

    changes

--- BSEB_MVC/Models/Subjects.cs
+++ BSEB_MVC/Models/Subjects.cs
@@ -14,7 +14,7 @@
 
         public List<Compulsarygrp1> Compulsarygrp1 { get; set; }
         public List<Compulsarygrp2> Compulsarygrp2 { get; set; }
-        public List<Eleactivegrp> Eleactivegrp { get; set; }
+        //public List<Eleactivegrp> Eleactivegrp { get; set; }
         public List<Additionalgrp> Additionalgrp { get; set; }
     }
     public class Compulsarygrp1

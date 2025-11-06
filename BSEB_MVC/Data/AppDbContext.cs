9c04d989de7a61708e854f994e6cba2dc849a222
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-11-06 17:58:20 +0530

    changes

--- BSEB_MVC/Data/AppDbContext.cs
+++ BSEB_MVC/Data/AppDbContext.cs
@@ -9,18 +9,20 @@ namespace BSEB_MVC.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
-
         public DbSet<FacultyMaster> Faculty_Mst { get; set; }
         public DbSet<StudentMaster> Student_Mst { get; set; }
         public DbSet<CollegeMaster> CollegeMaster { get; set; }
-        public DbSet<CollegeMaster> DeviceMaster { get; set; }
-        public DbSet<CollegeMaster>  Master { get; set; }
+        public DbSet<Student_PayMst> Student_PayMst { get; set; }
+        //public DbSet<CollegeMaster> CollegeMaster { get; set; }
+        //public DbSet<Student_PayMst> Student_PayMst { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // ✅ Mark StudentMaster as keyless for SP result mapping
             modelBuilder.Entity<FacultyMaster>().HasKey(f => f.Pk_FacultyId);
             modelBuilder.Entity<StudentMaster>().HasNoKey();
             modelBuilder.Entity<CollegeMaster>().HasNoKey();
+            modelBuilder.Entity<Student_PayMst>().HasNoKey();
         }
     }
 }

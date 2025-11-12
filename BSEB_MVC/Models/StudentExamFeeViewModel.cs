4c3e46726c687889976643bf90e0b3adc7aecdbc
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-11-12 17:27:08 +0530

    add new

---
+++ BSEB_MVC/Models/StudentExamFeeViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace BSEB_MVC.Models
+{
+    public class StudentExamFeeViewModel
+    {
+        public IEnumerable<College_Mst>? College_Mst { get; set; }
+        public IEnumerable<ExamType_Mst>? ExamType_Mst { get; set; }
+        public IEnumerable<FacultyMaster>? FacultyMaster { get; set; }
+        public IEnumerable<ExamDownloadStudentData>? ExamDownloadStudentData { get; set; }
+
+        public string? CollegeCode { get; set; }
+
+        //[Required(ErrorMessage = "Please select Faculty.")]
+        public int? SelectedFacultyId { get; set; }
+        public int? SelectedCollegeId { get; set; }
+        public int? SelectedExamTypeId { get; set; }
+
+        public IEnumerable<Student_mst>? Students { get; set; }
+    }
+}

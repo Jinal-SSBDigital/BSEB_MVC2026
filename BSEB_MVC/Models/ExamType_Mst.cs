539d9d92360735f7f4af8b0bfdfa0540e19642e2
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-11-12 17:26:37 +0530

    add new

---
+++ BSEB_MVC/Models/ExamType_Mst.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BSEB_MVC.Models
+{
+    public class ExamType_Mst
+    {
+        [Key]
+        public int Pk_ExamTypeId { get; set; }
+        public string ExamTypeName { get; set; }
+        public bool IsActive { get; set; }
+    }
+}

6d32b501e8be6ca55188b7193842cec7ddcbc89d
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-11-12 17:25:38 +0530

    add new

---
+++ BSEB_MVC/Models/ExamDownloadStudentData.cs
@@ -0,0 +1,13 @@
+﻿namespace BSEB_MVC.Models
+{
+    public class ExamDownloadStudentData
+    {
+        public int StudentID { get; set; }
+        public string? Name { get; set; }
+        public string? FatherName { get; set; }
+        public string? College { get; set; }
+        public string? Faculty { get; set; }
+        public string? ExamTypeName { get; set; }
+        public string? RegistrationNo { get; set; }
+    }
+}

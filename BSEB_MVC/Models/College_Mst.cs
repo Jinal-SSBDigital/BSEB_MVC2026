2d51c7105ff9cf471212b694eea2a1ef2f2b94db
Jinal Nagar (SSB Digital) <jinal.nagar@ssbi.in>
2025-11-13 17:04:41 +0530

    changes

---
+++ BSEB_MVC/Models/College_Mst.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BSEB_MVC.Models
+{
+    public class College_Mst
+    {
+        [Key]
+        public int Pk_CollegeId { get; set; }
+        public string? UserName { get; set; }
+        public string? CollegeName { get; set; }
+        public string? CollegeCode { get; set; }
+        public string? DistrictName { get; set; }
+        public string? DistrictCode { get; set; }
+        public string? PrincipalMobileNo { get; set; }
+        public string? EmailId { get; set; }
+    }
+}
